5830b861774425c58727fb3fddaa53b2e5c8dd74
Aadel Bouzambou <abouzambou@gmail.com>
2022-05-23 15:11:46 +0200

    seed added and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@ puts "Cleaning database..."
 Restaurant.destroy_all
 
 5.times do
-  Retaurant.create(
+  Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_name,
     phone_number: Faker::PhoneNumber.cell_phone,
-    category: %w["chinese", "italian", "japanese", "french", "belgian"].sample
+    category: %w[chinese italian japanese french belgian].sample
   )
 end
 
